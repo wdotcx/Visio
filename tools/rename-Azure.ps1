1282bc40c25202bc8526c99b7eaded1c850280d8
Paul Webb <bsdnet@gmail.com>
2024-04-05 08:27:52 +1100

    Optimise

--- tools/rename-Azure.ps1
+++ tools/rename-Azure.ps1
@@ -6,6 +6,6 @@ $directory = "."
 $svgFiles = Get-ChildItem -Path $directory -Recurse -Filter "*.svg"
 
 foreach ($file in $svgFiles) {
-    $newName = $file.Name -replace '^\d+-icon-service-', '' -replace '-', ' ' -replace '_', ' '
+    $newName = $file.Name -replace '^\d+-icon-service-', '' -replace '[-_]', ' '
     Rename-Item $file.FullName -NewName $newName
 }
