09ab10ad931aa76038e5bb1fcc2a2507b7f504fa
Paul Webb <bsdnet@gmail.com>
2024-04-05 08:07:57 +1100

    Initial commit

---
+++ tools/rename-Azure.ps1
@@ -0,0 +1,11 @@
+#
+# Rename Azure SVG files
+#
+
+$directory = "."
+$svgFiles = Get-ChildItem -Path $directory -Recurse -Filter "*.svg"
+
+foreach ($file in $svgFiles) {
+    $newName = $file.Name -replace '^\d+-icon-service-', '' -replace '-', ' ' -replace '_', ' '
+    Rename-Item $file.FullName -NewName $newName
+}
